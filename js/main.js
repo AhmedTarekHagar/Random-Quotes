ace4e3c501390bc41ff9d47d6643beb737d7b31f
AhmedTarekHagar <ahmed.tarek.ronaldo@gmail.com>
2023-12-06 05:56:17 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*
+-------- add array of objects (quotes)
+-------- loop on array
+-------- create html structure
+-------- pass html to document
+*/ 
+
+var quotes = [
+    {
+        quote: `“Be yourself; everyone else is already taken.”`,
+        author: `― Oscar Wilde`,
+        image: `images/1.jpg`
+    },
+    {
+        quote: `“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”`,
+        author: `― Marilyn Monroe`,
+        image: `images/2.jpg`
+    },
+    {
+        quote: `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`,
+        author: `― Albert Einstein`,
+        image: `images/3.jpg`
+    },
+    {
+        quote: `“A room without books is like a body without a soul.”`,
+        author: `― Marcus Tullius Cicero`,
+        image: `images/4.jpg`
+    },
+    {
+        quote: `“You've gotta dance like there's nobody watching,
+        Love like you'll never be hurt,
+        Sing like there's nobody listening,
+        And live like it's heaven on earth.”`,
+        author: `― William W. Purkey`,
+        image: `images/5.jpg`
+    }
+]
+
+
